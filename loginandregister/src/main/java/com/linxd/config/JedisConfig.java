b63d745d18820c4c0638a5c33ba0101580245b02
wzdlinxd <2661169807@qq.com>
2019-11-13 15:10:40 +0800

    创建子工程

---
+++ loginandregister/src/main/java/com/linxd/config/JedisConfig.java
@@ -0,0 +1,69 @@
+package com.linxd.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author linxd
+ * @date 2019/11/6 11:48
+ * Description: No Description
+ */
+@Slf4j
+@Configuration
+public class JedisConfig {
+    @Value("${spring.redis.host}")
+    public String host;
+
+    @Value("${spring.redis.port}")
+    public Integer port;
+
+    @Value("${spring.redis.password}")
+    public String password;
+
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+
+    @Value("${spring.redis.jedis.pool.max-idle}")
+    private int maxIdle;
+
+    @Value("${spring.redis.jedis.pool.max-wait}")
+    private long maxWaitMillis;
+
+    @Value("${spring.redis.block-when-exhausted}")
+    private boolean  blockWhenExhausted;
+
+    public JedisConfig() {
+        int i = 0;
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.redis")
+    JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        log.info(password);
+        jedisConnectionFactory.setPassword(password);
+        return jedisConnectionFactory;
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.redis.jedis.pool")
+    public JedisPool jedisPool() {
+        log.info("JedisPool注入成功！！");
+        log.info("redis地址：" + host + ":" + port);
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+        // 连接耗尽时是否阻塞, false报异常,ture阻塞直到超时, 默认true
+        jedisPoolConfig.setBlockWhenExhausted(blockWhenExhausted);
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port);
+        return jedisPool;
+    }
+}
